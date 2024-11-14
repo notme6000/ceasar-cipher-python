422f05336414334972272288a8f44efea18ed844
notme6000 <sureshjithu9400@gmail.com>
2024-11-14 16:28:33 +0530

    user input

--- ceasar-cipher.py
+++ ceasar-cipher.py
@@ -1,1 +1,39 @@
-print("hello world")
+print("ceasar cipher")
+
+
+
+def user_input():
+    code = " "
+    i = 0
+    code = input("enter the code to encrypt:")
+    code_len = len(code)
+    for i in range(code_len):
+        code_new = i
+        encryption(code_new)
+
+    
+
+
+user_input()
+
+
+def encryption(code_new):
+    
+    encrypt_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    i = 0
+    if code_new.islower():
+        pass
+    else:
+        lower_case_code = code_new.lower()
+    
+    for i in range(encrypt_list):
+        if encrypt_list[i] == lower_case_code:
+            print(i)
+            i+=1
+
+    
+
+
+
+# def decryption():
+    
